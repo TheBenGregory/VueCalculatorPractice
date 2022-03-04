c5c58793eb8f1f9f143ab5201fde979f67ade502
Ben Gregory <JBGregoryPro@protonmail.com>
2022-03-04 11:46:08 -0600

    initial buildout plus first operands

---
+++ src/components/Calculator.vue
@@ -0,0 +1,86 @@
+
+   
+<template>
+  <div class="calculator">
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div @click="divide" class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="times" class="btn operator">x</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="minus" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="add" class="btn operator">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn">.</div>
+    <div @click="equal" class="btn operator">=</div>
+  </div>
+</template>
+
+   
+<script>
+export default {
+  name: 'calc-data',
+  data() {
+    return {
+      current: '',
+    }
+  },
+  methods: {
+    clear() {
+      this.current = ''
+    },
+    sign() {
+      this.current = this.current.charAt(0) === '-' ? this.current.slice(1) 
+      : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      this.current = `${this.current}` + `${number}`
+    },
+    dot() {
+      if (this.current.indexOf('.') === -1) {
+      this.append('.')}
+    }
+  }
+};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.calculator {
+  width: 250px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+  
+}
+.display {
+  grid-column: 1 / 5;
+  background: rgb(164, 164, 214);
+}
+.zero {
+  grid-column: 1 /3;
+}
+.btn {
+  background-color: aliceblue;
+  border: 1px solid rgb(49, 61, 83)
+}
+
+.operator {
+  background-color: rgb(221, 188, 105);
+  color: aliceblue;
+}
+</style>
